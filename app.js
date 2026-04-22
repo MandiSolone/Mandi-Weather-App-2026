88a8789d27c7346307a41762348a2926275aea5c
mandi solone <mandisolone@mandis-mbp.lan>
2026-04-22 19:54:00 -0400

    added logic for saved cities, added local cache

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const locationKeyCache = {};
 console.log("locationKeyCache:", locationKeyCache);
 
 // Private AccuWeather API Key // Replace with "YOUR_API_KEY" for Github //
-const API_KEY = "zpka_02429ca4d5034d448049446498632f6c_3f7104b2";
+const API_KEY = "YOUR_API_KEY";
 
 // Loading Spinner Icon //
 const spinner = document.getElementById("spinner");
